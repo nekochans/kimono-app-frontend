775b33a3aa1fa3bcf2085710ce5e3c458d76303e
keitakn <keita.koga.work@gmail.com>
2020-05-01 18:16:48 +0900

    :sparkles: #3 サーバーサイド側でCookieをセットして、それを読み込む為のサンプルコードを追加

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { NextPageContext } from 'next';
+import Link from 'next/link';
+import styled from 'styled-components';
 import { setCookie } from '../infrastructure/Cookie';
 
 type Props = {
   authToken: string;
 };
 
+const StyledLink = styled.a`
+  color: blue;
+  text-decoration: underline;
+`;
+
 const AuthPage: React.FC<Props> = ({ authToken }: Props) => {
-  return <p>Auth Token is... {authToken}</p>;
+  return (
+    <>
+      <p>Set Auth Token in a Cookie AuthToken is...[{authToken}]</p>
+      <p>
+        <Link href="/authed">
+          <StyledLink>Go To authed Page.</StyledLink>
+        </Link>
+      </p>
+    </>
+  );
 };
 
 export const getServerSideProps = async (ctx: NextPageContext) => {
