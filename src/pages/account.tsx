5bb47f5e187d6dcfcdc08e4af7b27a6e6348598a
kobayashi-m42 <m42kobayashi@gmail.com>
2020-07-10 23:57:22 +0900

    :sparkles: #5 local stateにログイン情報を保持する処理を追加

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,44 +1,26 @@
 import React from 'react';
-import {
-  AmplifyAuthenticator,
-  AmplifySignUp,
-  AmplifySignOut,
-} from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 
-const IndexPage: React.FC = () => {
-  return (
-    <AmplifyAuthenticator>
-      <AmplifySignUp
-        headerText="Sign Up"
-        slot="sign-up"
-        usernameAlias="username"
-        formFields={[
-          {
-            type: 'username',
-            label: 'ユーザ名(必須)',
-            placeholder: '',
-            required: true,
-          },
-          {
-            type: 'email',
-            label: 'メールアドレス(必須)',
-            placeholder: '',
-            required: true,
-          },
-          {
-            type: 'password',
-            label: 'パスワード(必須)',
-            placeholder: '',
-            required: true,
-          },
-        ]}
-      />
-      <div>
-        Kimono APP
-        <AmplifySignOut />
-      </div>
-    </AmplifyAuthenticator>
+const AuthStateApp: React.FunctionComponent = () => {
+  const [authState, setAuthState] = React.useState<AuthState>();
+  const [user, setUser] = React.useState<object | undefined>();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <div className="App">
+      <div>Hello, {user.username}</div>
+      <AmplifySignOut />
+    </div>
+  ) : (
+    <AmplifyAuthenticator />
   );
 };
 
-export default IndexPage;
+export default AuthStateApp;
