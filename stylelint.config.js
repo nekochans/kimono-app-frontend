b07bbcae0abb4f17447af62665e3d8c13c4d28dd
keitakn <keita.koga.work@gmail.com>
2020-10-25 23:55:56 +0900

    :wrench: #25 stylelintの設定の一部が無効になっていたので極力シンプルなルールに変更

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,40 +1,17 @@
-const argv = require('yargs').argv;
-const glob = argv['_'] && argv['_'][0];
-const isJsxFile = glob && /.{jsx,tsx}/.test(glob);
-
-if (isJsxFile) {
-  module.exports = {
-    extends: [
-      'stylelint-config-standard',
-      './node_modules/prettier-stylelint/config.js',
-    ],
-    plugins: ['stylelint-order'],
-    rules: {
-      'declaration-empty-line-before': 'never',
-      'indentation': 2,
-      'no-missing-end-of-source-newline': null,
-      'string-quotes': 'single',
-      'order/properties-alphabetical-order': true
-    }
-  };
-}
-
 module.exports = {
   extends: [
     'stylelint-config-standard',
-    './node_modules/prettier-stylelint/config.js'
+    'stylelint-config-recess-order',
+  ],
+  plugins: [
+    'stylelint-order',
   ],
   ignoreFiles: [
     '**/node_modules/**',
-    'src/styles/bundle.css'
+    'src/styles/bundle.css',
   ],
-  plugins: ['stylelint-order'],
   rules: {
-    'declaration-empty-line-before': 'never',
-    'indentation': 2,
-    'no-missing-end-of-source-newline': null,
     'string-quotes': 'single',
-    'order/properties-alphabetical-order': true,
     'at-rule-no-unknown': [
       true,
       { ignoreAtRules: ['tailwind'] }
