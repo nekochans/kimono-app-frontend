85816112e6324e03f17c16ec1aa501b5d3fc4fb9
keitakn <keita.koga.work@gmail.com>
2020-10-26 23:23:02 +0900

    :wrench: #25 jestの設定ファイルで非推奨になっている書き方が残っていたので修正

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
     // to transform jsx into js rather than leaving it jsx such as the next build requires. you
     // can see this setting in tsconfig.jest.json -> "jsx": "react"
     'ts-jest': {
-      'tsConfig': '<rootDir>/test/tsconfig.jest.json'
+      'tsconfig': '<rootDir>/test/tsconfig.jest.json'
     }
   }
 };
