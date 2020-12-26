fcbf0f013213729136cd2cda9a4abb85062dbc21
keitakn <keita.koga.work@gmail.com>
2020-12-27 00:56:13 +0900

    :recycle: #37 Amplifyの設定をdomain配下に移動

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,30 +3,9 @@ import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import Amplify from 'aws-amplify';
 import createStore from '../redux/createStore';
+import { amplifyConfig } from '../domain/cognito/environmentVariable';
 
-Amplify.configure({
-  Auth: {
-    region: 'ap-northeast-1',
-    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
-    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_WEB_CLIENT_ID,
-    mandatorySignIn: false,
-    authenticationFlowType: 'USER_SRP_AUTH',
-    cookieStorage: {
-      domain: process.env.NEXT_PUBLIC_AUTH_COOKIE_DOMAIN,
-      path: '/',
-      expires: 30,
-      secure: process.env.NEXT_PUBLIC_AUTH_COOKIE_DOMAIN !== 'localhost',
-    },
-  },
-  oauth: {
-    domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
-    scope: ['profile', 'email', 'openid'],
-    redirectSignIn: `${String(process.env.NEXT_PUBLIC_APP_URL)}/idp/callback`,
-    redirectSignOut: `${String(process.env.NEXT_PUBLIC_APP_URL)}/idp/logout`,
-    responseType: 'code',
-  },
-  ssr: true,
-});
+Amplify.configure(amplifyConfig());
 
 const kimonoApp = ({ Component, pageProps }: AppProps): ReactNode => {
   return (
