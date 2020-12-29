2544582fb8f36f669c8ea0e41f437e6b244fb49f
keitakn <keita.koga.work@gmail.com>
2020-12-29 11:51:36 +0900

    :white_check_mark: #37 JWKの取得に失敗した際のテストコードを追加

--- src/infrastructure/repository/__test__/CognitoJwkRepository.spec.ts
+++ src/infrastructure/repository/__test__/CognitoJwkRepository.spec.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import fetch from 'jest-fetch-mock';
 import { fetchCognitoJsonWebKeys } from '../CognitoJwkRepository';
+import FetchCognitoJsonWebKeysError from '../../../domain/error/cognito/FetchCognitoJsonWebKeysError';
 
 describe('CognitoJwkRepository.ts Functions TestCases', () => {
   beforeEach(() => {
@@ -39,5 +40,22 @@ describe('CognitoJwkRepository.ts Functions TestCases', () => {
     const jwkList = await fetchCognitoJsonWebKeys();
 
     expect(jwkList).toStrictEqual(mockBody);
+  });
+
+  it('should return an Error because the HTTP status is not 200', async () => {
+    const mockBody = { message: 'Internal Server Error' };
+
+    const mockParams = {
+      status: 500,
+      statusText: 'Internal Server Error',
+    };
+
+    fetch.mockResponseOnce(JSON.stringify(mockBody), mockParams);
+
+    const jwkListPromise = fetchCognitoJsonWebKeys();
+
+    await expect(jwkListPromise).rejects.toThrowError(
+      FetchCognitoJsonWebKeysError,
+    );
   });
 });
